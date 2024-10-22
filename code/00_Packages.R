8ef56d5dfa18847fffda71d48260d1f94d9ad952
Andreas Schwarz Meyer <andreas.ls.meyer@gmail.com>
2024-10-22 18:18:45 +0200

    .

--- code/00_Packages.R
+++ code/00_Packages.R
@@ -1,11 +1,5 @@
 if(!requireNamespace("pacman", quietly = TRUE)) install.packages("pacman")
 
-# pacman::p_load(colorspace, cowplot, cowplot, exactextractr, furrr,
-#                fuzzyjoin, geomtextpath, ggdensity, ggdist, ggExtra, ggnewscale,
-#                ggpattern, ggpointdensity, ggtext, ggthemes, here, metR, parallel, 
-#                patchwork, pbapply, png, raster, readxl, rnaturalearth, rphylopic,
-#                scales, sf, stringr, tidyverse, viridis)
-
 pacman::p_load(colorspace, exactextractr, furrr, ggdist, 
                ggthemes, here, parallel, patchwork, pbapply, 
                phyloregion, phytools, readxl, rlang, rnaturalearth, rphylopic, 
