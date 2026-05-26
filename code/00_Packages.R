36663ae8539ad19c5626d6161db51183af727b52
schwarzmeyer <andreas.ls.meyer@gmail.com>
2026-05-26 14:52:33 +0200

    update packages

--- code/00_Packages.R
+++ code/00_Packages.R
@@ -4,10 +4,5 @@ library(pacman)
 p_load(here, scales, tictoc, scico, glue, zoo, rnaturalearth, exactextractr, pbmcapply, rphylopic, readxl,
        colorspace, ggdist, cowplot, patchwork, tidyterra, terra, sf, tidyverse, rstatix, furrr, ggtext)
 
-# p_load(arrow, tictoc, colorspace, cowplot, exactextractr, furrr, ggdist, ggExtra,
-#        ggpointdensity, ggpubr, ggthemes, glue, here, pbmcapply, parallel, patchwork, 
-#        pbapply, phyloregion, phytools, readxl, rlang, rnaturalearth, 
-#        rphylopic, scales, scico, sf, terra, tidyverse, zoo)
-
 gc()
 
